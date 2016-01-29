9e045a3a0cd341233731515bc4082a9933144e9b
Roman Nikitin <NikRom@macbook-air-roman.local>
2016-01-29 14:21:52 +0300

    Added button class Added new scene

---
+++ Island Empire/NewGameScene.swift
@@ -0,0 +1,22 @@
+//
+//  NewGameScene.swift
+//  Island Empire
+//
+//  Created by Roman Nikitin on 29.01.16.
+//  Copyright © 2016 theSuperb. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class NewGameScene: SKScene {
+    
+    
+    override func didMoveToView(view: SKView) {
+        <#code#>
+    }
+    
+    override func update(currentTime: NSTimeInterval) {
+        
+    }
+}
