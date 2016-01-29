9e47873b17d00bc577dd0e5e1ac00bbeb46b57c4
Roman Nikitin <NikRom@macbook-air-roman.local>
2016-01-29 13:45:46 +0300

    Added main island class

---
+++ Island Empire/MainIslandClass.swift
@@ -0,0 +1,14 @@
+//
+//  MainIslandClass.swift
+//  Island Empire
+//
+//  Created by Roman Nikitin on 29.01.16.
+//  Copyright © 2016 theSuperb. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class Island: SKSpriteNode {
+    // ПИШИ
+}
